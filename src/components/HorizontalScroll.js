cac5a4ae03d46b592b784f2daa6343527c9263f4
Nguedia Adele <nguediaadele@yahoo.fr>
2019-07-11 11:25:35 +0100

    First commit

---
+++ src/components/HorizontalScroll.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import MovieTile from './MovieTile'
+
+class HorizontalScroll extends React.Component{
+	render(){
+		return(
+			<div>
+				<MovieTile/>
+			</div>
+		)
+	}
+}
+
+export default HorizontalScroll
---
+++ src/components/MovieTile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MovieTile extends React.Component{
+	render(){
+		return(
+			<div>movie tile</div>
+		)
+	}
+}
+
+export default MovieTile
