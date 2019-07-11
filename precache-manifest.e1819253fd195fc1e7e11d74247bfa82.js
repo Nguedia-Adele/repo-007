a5bd18c3cbe412e30867475050735140570dcb18
Nguedia Adele <nguediaadele@yahoo.fr>
2019-07-11 11:21:25 +0100

    Updates

---
+++ precache-manifest.e1819253fd195fc1e7e11d74247bfa82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46c5304d68fda4a23bee1f695dec9f7",
+    "url": "/repo-007/index.html"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/repo-007/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4727e688f30d13749fcd",
+    "url": "/repo-007/static/css/main.94ad8322.chunk.css"
+  },
+  {
+    "revision": "d7df286706c6c6ab1324",
+    "url": "/repo-007/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "4727e688f30d13749fcd",
+    "url": "/repo-007/static/js/main.325d8e45.chunk.js"
+  },
+  {
+    "revision": "4ed04338c9bebf06e8c4",
+    "url": "/repo-007/static/js/runtime~main.de91ed84.js"
+  }
+]);
